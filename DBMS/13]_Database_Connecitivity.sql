23dac5098109661eb607853350db84022f1102ff
Root <sweetrat6163@gmail.com>
2025-11-08 22:03:31 +0530

    Create 13]_Database_Connecitivity.sql

---
+++ DBMS/13]_Database_Connecitivity.sql
@@ -0,0 +1,3 @@
+"
+13  Database Connectivity: Write a program to implement MongoDB database connectivity with any front end language to implement Database navigation operations (add, delete, edit etc.)s
+"
