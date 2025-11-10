cb92058f7fd4b17391d70f91db9fa2611f423ab6
Root <sweetrat6163@gmail.com>
2025-11-10 14:40:27 +0530

    Add files via upload

---
+++ SPOS/4/Ass4_FCFS.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Ass4_FCFS {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of processes: ");
+        int n = sc.nextInt();
+        int pid[] = new int[n], ar[] = new int[n], bt[] = new int[n], ct[] = new int[n], ta[] = new int[n], wt[] = new int[n];
+        float avgwt = 0, avgta = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter process " + (i + 1) + " arrival time and burst time: ");
+            ar[i] = sc.nextInt();
+            bt[i] = sc.nextInt();
+            pid[i] = i + 1;
+        }
+
+        // Sort processes by arrival time
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (ar[j] > ar[j + 1]) {
+                    swap(ar, j);
+                    swap(bt, j);
+                    swap(pid, j);
+                }
+            }
+        }
+
+        // Calculate completion, turnaround, waiting time
+        for (int i = 0; i < n; i++) {
+            if (i == 0) ct[i] = ar[i] + bt[i];
+            else ct[i] = Math.max(ar[i], ct[i - 1]) + bt[i];
+            ta[i] = ct[i] - ar[i];
+            wt[i] = ta[i] - bt[i];
+            avgwt += wt[i];
+            avgta += ta[i];
+        }
+
+        System.out.println("\nPid\tArrival\tBurst\tComplete\tTurnaround\tWaiting");
+        for (int i = 0; i < n; i++) {
+            System.out.println(pid[i] + "\t" + ar[i] + "\t" + bt[i] + "\t" + ct[i] + "\t\t" + ta[i] + "\t\t" + wt[i]);
+        }
+
+        System.out.println("\nAverage waiting time: " + avgwt / n);
+        System.out.println("Average turnaround time: " + avgta / n);
+        sc.close();
+    }
+
+    static void swap(int arr[], int i) {
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+    }
+}
+
